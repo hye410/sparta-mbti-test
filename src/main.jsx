f09396eae19be6e9f8d963239d737196b34ccb44
Hye10 <110617039+hye410@users.noreply.github.com>
2025-02-23 20:39:38 +0900

    [chore] tanstack devtools 제거

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,14 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./css/global.css";
 import App from "./App.jsx";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import "./css/global.css";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={false} />
       <App />
     </QueryClientProvider>
   </StrictMode>
