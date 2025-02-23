0cdd91aa40309e4ee63018a4dbf4d4ffd2f37065
Hye10 <110617039+hye410@users.noreply.github.com>
2025-02-23 13:36:26 +0900

    [feat] 닉네임 변경 후 클라이언트 상태에 바로 반영 추가

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,22 +1,26 @@
 import { updateProfile } from "../api/profile";
 import Form from "../components/common/Form";
+import useUserStore from "../zustand/userStore";
 
-const profileFormData = [
+const profileFormData = (initNickname) => [
   {
     id: "nickname",
     label: "닉네임",
     type: "text",
-    initValue: "",
+    initValue: initNickname,
     placeholder: "닉네임",
     autoFocus: true,
   },
 ];
 
 export default function MyPage() {
+  const { user, setUser } = useUserStore((state) => state);
   const handleChangeProfile = async (e, newProfile) => {
     e.preventDefault();
     try {
-      await updateProfile(newProfile);
+      const res = await updateProfile(newProfile);
+      setUser(newProfile);
+      alert(res.message);
     } catch (error) {
       console.error(error);
       alert(error);
@@ -27,7 +31,7 @@ export default function MyPage() {
       <section className="form">
         <h3 className="h3">프로필 수정</h3>
         <Form
-          formData={profileFormData}
+          formData={profileFormData(user.nickname ?? "")}
           handleSubmit={handleChangeProfile}
           submitButton="변경"
         />
