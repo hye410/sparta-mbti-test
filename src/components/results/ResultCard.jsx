6a6766fe0eb86f3e818b74f62ddf15fe9509742c
Hye10 <110617039+hye410@users.noreply.github.com>
2025-02-24 14:41:21 +0900

    [design] 스마트폰 가로보기도 반응형 되도록 수정

--- src/components/results/ResultCard.jsx
+++ src/components/results/ResultCard.jsx
@@ -70,7 +70,7 @@ export default function ResultCard({ result }) {
   };
 
   return (
-    <section className="w-[70%] max-w-[600px] min-w-[300px] bg-white px-[2vw] py-[3vh] mx-[auto] my-[30px] rounded-md shadow-md">
+    <section className="w-[70%] max-w-[600px] min-w-[300px] bg-white px-[5vw] py-[4vw] lg:px-[2vw] lg:py-[3vh] mx-[auto] my-[30px] rounded-md shadow-md">
       <div className="flex items-center justify-between pb-[20px] mb-[20px] border-b-2 border-b-slate-700">
         <p className="text-xl  font-bold ">{result.nickname}</p>
         <span className="text-xs">{result.date}</span>
