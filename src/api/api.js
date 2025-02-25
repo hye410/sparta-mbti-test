7eb80bd89f7b2213c334324a86ff75c212a5dea0
Hye10 <110617039+hye410@users.noreply.github.com>
2025-02-25 14:52:03 +0900

    [refactor] 서버 통신 시 interceptor해서 토큰 값을 저장 / 요청 시에도 interceptor로 header에 토큰 주입

--- src/api/api.js
+++ src/api/api.js
@@ -7,14 +7,29 @@ export const api = axios.create({
   baseURL: import.meta.env.VITE_SERVER_API,
 });
 
-// 통신 도중 오류 발생 시 서버에서 내려오는 code와 message만 에러로 던져줌
-api.interceptors.response.use(null, (reject) => {
-  const { data, status } = reject.response;
-  throw {
-    code: status || "unknown code",
-    message: data?.message || "오류가 발생했습니다.",
-  };
+api.interceptors.request.use((config) => {
+  const token = sessionStorage.getItem("accessToken");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
+
+api.interceptors.response.use(
+  (response) => {
+    const { accessToken } = response.data;
+    if (accessToken) sessionStorage.setItem("accessToken", accessToken);
+    delete response.data?.accessToken;
+    return response;
+  },
+  (reject) => {
+    const { data, status } = reject.response;
+    throw {
+      code: status || "unknown code",
+      message: data?.message || "오류가 발생했습니다.",
+    };
+  }
+);
 
 /**
  * json-server와 연결된 api / 테스트 등록, 수정, 삭제를 처리
